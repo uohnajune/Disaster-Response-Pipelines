6d7b7d38caaf8f6695739133a0a8730968df1b0d
Uohna June Thiessen <uohnajune@gmail.com>
2020-12-12 20:08:51 +0000

    added load_data function

---
+++ models/train_classifier.py
@@ -0,0 +1,67 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(database_filepath):
+
+    # Make database engine.
+    engine = create_engine('sqlite:///' + database_filepath)
+
+    # Load database file into data table.
+    data_table = pd.read_sql_table('messages', engine)
+
+    # Break data table into messages, categories, and column names.
+    messages = data_table['message']
+    categories = data_table.drop(columns=['id','message','original','genre'])
+    category_names = categories.columns
+
+    return messages, categories, category_names
+
+
+def tokenize(text):
+    pass
+
+
+def build_model():
+    pass
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    pass
+
+
+def save_model(model, model_filepath):
+    pass
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
